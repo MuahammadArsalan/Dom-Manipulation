2d299904bc72d2ce84ae3c610f54d24c28390d13
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-03-04 14:09:40 +0530

    Dom-Manipulation

---
+++ app.js
@@ -0,0 +1,45 @@
+// var h1 = document.querySelector('.head');
+// h1.innerHTML = "Changed through JS"
+// console.log(h1.innerHTML);
+// var paragraph = document.querySelector('.para');
+// paragraph.innerHTML= "Hey! .I am a para."
+
+
+
+
+// var h1 = document.querySelector(".head")
+// function changeText(){
+//     if(h1.innerHTML=="Hello JS" ){
+//         h1.innerHTML="Change through Js"
+//     }else{
+//         h1.innerHTML="Hello JS"
+//     }
+// console.log(h1.innerHTML)
+// }
+
+
+
+
+// var input =document.querySelector(".input")
+// function getInputUser(){
+//     console.log(input.value)
+//     console.log(input.value = " ")
+// }
+
+
+// var firstName = `Muhammad `
+// var lastName = `Arsalan`
+// var age = 18;
+// console.log(`My name is ${firstName} ${lastName} and my age is ${age}`)
+
+
+
+
+
+
+
+
+
+
+
+
